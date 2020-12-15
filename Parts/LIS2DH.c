7bf811fe0e2bd578e53d4463394d1310af8862c5
Tim Lamborn <tlamborn@gmail.com>
2020-12-16 03:04:15 +1300

    Added LIS2 stub

---
+++ Parts/LIS2DH.c
@@ -0,0 +1,33 @@
+
+#include "GPIO.h"
+#include "SPI.h"
+
+/*
+ * PRIVATE DEFINITIONS
+ */
+
+/*
+ * PRIVATE TYPES
+ */
+
+/*
+ * PRIVATE PROTOTYPES
+ */
+
+/*
+ * PRIVATE VARIABLES
+ */
+
+/*
+ * PUBLIC FUNCTIONS
+ */
+
+
+
+/*
+ * PRIVATE FUNCTIONS
+ */
+
+/*
+ * INTERRUPT ROUTINES
+ */
---
+++ Parts/LIS2DH.h
@@ -0,0 +1,21 @@
+#ifndef LIS2DH_H
+#define LIS2DH_H
+
+#include "Board.h"
+
+/*
+ * PUBLIC DEFINITIONS
+ */
+
+/*
+ * PUBLIC TYPES
+ */
+
+/*
+ * PUBLIC FUNCTIONS
+ */
+
+void LIS2_Init(void);
+void LIS2_Deinit(void);
+
+#endif //LIS2DH_H
